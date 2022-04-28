2dae807f03654bb6b220424e473277b1314d19f2
Hugging Face Doc Builder <docs@huggingface.co>
2022-04-28 15:09:07 +0000

    Updated with commit 11e8b332172a97990dde348e477ffadadd7929cc See: https://github.com/huggingface/accelerate/commit/11e8b332172a97990dde348e477ffadadd7929cc

---
+++ accelerate/v0.7.0/en/_app/pages/checkpoint.mdx-f5560298.js
@@ -0,0 +1,67 @@
+import{S as Be,i as Ne,s as Te,e as s,k as d,w as Re,t as r,M as De,c as o,d as a,m as h,a as n,x as qe,h as i,b as f,F as t,g as p,y as xe,L as Ge,q as Ce,o as Ie,B as Ue,v as Oe}from"../chunks/vendor-4918fc3c.js";import{I as Me}from"../chunks/IconCopyLink-21d338b1.js";import{C as We}from"../chunks/CodeBlock-99419108.js";function Fe(fe){let u,B,_,g,P,w,K,R,Q,N,E,V,T,v,k,X,j,Y,Z,ee,y,te,A,ae,se,q,oe,D,$,re,G,z,l,ne,S,ie,ce,x,le,pe,C,me,de,I,he,ue,O,L,_e,M,b,W;return w=new Me({}),b=new We({props:{code:`from accelerate import Accelerator
+import torch
+
+accelerator = Accelerator()
+
+my_scheduler = torch.optim.lr_scheduler.StepLR(my_optimizer, step_size=1, gamma=0.99)
+my_model, my_optimizer, my_training_dataloader = accelerate.prepare(my_model, my_optimizer, my_training_dataloader)
+
+# Register the LR scheduler
+accelerate.register_for_checkpointing(my_scheduler)
+
+# Save the starting state
+accelerate.save_state("my/save/path")
+
+device = accelerator.device
+my_model.to(device)
+
+# Perform training
+for epoch in range(num_epochs):
+    for batch in my_training_dataloader:
+        my_optimizer.zero_grad()
+        inputs, targets = batch
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        outputs = my_model(inputs)
+        loss = my_loss_function(outputs, targets)
+        accelerator.backward(loss)
+        my_optimizer.step()
+    my_scheduler.step()
+
+# Restore previous state
+accelerate.load_state("my/save/path")`,highlighted:`<span class="hljs-keyword">from</span> accelerate <span class="hljs-keyword">import</span> Accelerator
+<span class="hljs-keyword">import</span> torch
+
+accelerator = Accelerator()
+
+my_scheduler = torch.optim.lr_scheduler.StepLR(my_optimizer, step_size=<span class="hljs-number">1</span>, gamma=<span class="hljs-number">0.99</span>)
+my_model, my_optimizer, my_training_dataloader = accelerate.prepare(my_model, my_optimizer, my_training_dataloader)
+
+<span class="hljs-comment"># Register the LR scheduler</span>
+accelerate.register_for_checkpointing(my_scheduler)
+
+<span class="hljs-comment"># Save the starting state</span>
+accelerate.save_state(<span class="hljs-string">&quot;my/save/path&quot;</span>)
+
+device = accelerator.device
+my_model.to(device)
+
+<span class="hljs-comment"># Perform training</span>
+<span class="hljs-keyword">for</span> epoch <span class="hljs-keyword">in</span> <span class="hljs-built_in">range</span>(num_epochs):
+    <span class="hljs-keyword">for</span> batch <span class="hljs-keyword">in</span> my_training_dataloader:
+        my_optimizer.zero_grad()
+        inputs, targets = batch
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        outputs = my_model(inputs)
+        loss = my_loss_function(outputs, targets)
+        accelerator.backward(loss)
+        my_optimizer.step()
+    my_scheduler.step()
+
+<span class="hljs-comment"># Restore previous state</span>
+accelerate.load_state(<span class="hljs-string">&quot;my/save/path&quot;</span>)`}}),{c(){u=s("meta"),B=d(),_=s("h1"),g=s("a"),P=s("span"),Re(w.$$.fragment),K=d(),R=s("span"),Q=r("Checkpointing"),N=d(),E=s("p"),V=r(`When training a PyTorch model with Accelerate, you may often want to save and continue a state of training. Doing so requires
+saving and loading the model, optimizer, RNG generators, and the GradScaler. Inside Accelerate are two convience functions to achieve this quickly:`),T=d(),v=s("ul"),k=s("li"),X=r("Use "),j=s("a"),Y=r("save_state()"),Z=r(" for saving everything mentioned above to a folder location"),ee=d(),y=s("li"),te=r("Use "),A=s("a"),ae=r("load_state()"),se=r(" for loading everything stored from an earlier "),q=s("code"),oe=r("save_state"),D=d(),$=s("p"),re=r("It should be noted that the expectation is that those states come from the same training script, they should not be from two separate scripts."),G=d(),z=s("ul"),l=s("li"),ne=r("By using "),S=s("a"),ie=r("register_for_checkpointing()"),ce=r(`, you can register custom objects to be automatically stored or loaded from the two prior functions,
+so long as the object has a `),x=s("code"),le=r("state_dict"),pe=d(),C=s("strong"),me=r("and"),de=r(" a "),I=s("code"),he=r("load_state_dict"),ue=r(" functionality. This could include objects such as a learning rate scheduler."),O=d(),L=s("p"),_e=r("Below is a brief example using checkpointing to save and reload a state during training:"),M=d(),Re(b.$$.fragment),this.h()},l(e){const c=De('[data-svelte="svelte-1phssyn"]',document.head);u=o(c,"META",{name:!0,content:!0}),c.forEach(a),B=h(e),_=o(e,"H1",{class:!0});var F=n(_);g=o(F,"A",{id:!0,class:!0,href:!0});var ge=n(g);P=o(ge,"SPAN",{});var ve=n(P);qe(w.$$.fragment,ve),ve.forEach(a),ge.forEach(a),K=h(F),R=o(F,"SPAN",{});var ye=n(R);Q=i(ye,"Checkpointing"),ye.forEach(a),F.forEach(a),N=h(e),E=o(e,"P",{});var we=n(E);V=i(we,`When training a PyTorch model with Accelerate, you may often want to save and continue a state of training. Doing so requires
+saving and loading the model, optimizer, RNG generators, and the GradScaler. Inside Accelerate are two convience functions to achieve this quickly:`),we.forEach(a),T=h(e),v=o(e,"UL",{});var H=n(v);k=o(H,"LI",{});var J=n(k);X=i(J,"Use "),j=o(J,"A",{href:!0});var ke=n(j);Y=i(ke,"save_state()"),ke.forEach(a),Z=i(J," for saving everything mentioned above to a folder location"),J.forEach(a),ee=h(H),y=o(H,"LI",{});var U=n(y);te=i(U,"Use "),A=o(U,"A",{href:!0});var be=n(A);ae=i(be,"load_state()"),be.forEach(a),se=i(U," for loading everything stored from an earlier "),q=o(U,"CODE",{});var Ee=n(q);oe=i(Ee,"save_state"),Ee.forEach(a),U.forEach(a),H.forEach(a),D=h(e),$=o(e,"P",{});var je=n($);re=i(je,"It should be noted that the expectation is that those states come from the same training script, they should not be from two separate scripts."),je.forEach(a),G=h(e),z=o(e,"UL",{});var Ae=n(z);l=o(Ae,"LI",{});var m=n(l);ne=i(m,"By using "),S=o(m,"A",{href:!0});var $e=n(S);ie=i($e,"register_for_checkpointing()"),$e.forEach(a),ce=i(m,`, you can register custom objects to be automatically stored or loaded from the two prior functions,
+so long as the object has a `),x=o(m,"CODE",{});var ze=n(x);le=i(ze,"state_dict"),ze.forEach(a),pe=h(m),C=o(m,"STRONG",{});var Se=n(C);me=i(Se,"and"),Se.forEach(a),de=i(m," a "),I=o(m,"CODE",{});var Le=n(I);he=i(Le,"load_state_dict"),Le.forEach(a),ue=i(m," functionality. This could include objects such as a learning rate scheduler."),m.forEach(a),Ae.forEach(a),O=h(e),L=o(e,"P",{});var Pe=n(L);_e=i(Pe,"Below is a brief example using checkpointing to save and reload a state during training:"),Pe.forEach(a),M=h(e),qe(b.$$.fragment,e),this.h()},h(){f(u,"name","hf:doc:metadata"),f(u,"content",JSON.stringify(He)),f(g,"id","checkpointing"),f(g,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),f(g,"href","#checkpointing"),f(_,"class","relative group"),f(j,"href","/docs/accelerate/v0.7.0/en/accelerator#accelerate.Accelerator.save_state"),f(A,"href","/docs/accelerate/v0.7.0/en/accelerator#accelerate.Accelerator.load_state"),f(S,"href","/docs/accelerate/v0.7.0/en/accelerator#accelerate.Accelerator.register_for_checkpointing")},m(e,c){t(document.head,u),p(e,B,c),p(e,_,c),t(_,g),t(g,P),xe(w,P,null),t(_,K),t(_,R),t(R,Q),p(e,N,c),p(e,E,c),t(E,V),p(e,T,c),p(e,v,c),t(v,k),t(k,X),t(k,j),t(j,Y),t(k,Z),t(v,ee),t(v,y),t(y,te),t(y,A),t(A,ae),t(y,se),t(y,q),t(q,oe),p(e,D,c),p(e,$,c),t($,re),p(e,G,c),p(e,z,c),t(z,l),t(l,ne),t(l,S),t(S,ie),t(l,ce),t(l,x),t(x,le),t(l,pe),t(l,C),t(C,me),t(l,de),t(l,I),t(I,he),t(l,ue),p(e,O,c),p(e,L,c),t(L,_e),p(e,M,c),xe(b,e,c),W=!0},p:Ge,i(e){W||(Ce(w.$$.fragment,e),Ce(b.$$.fragment,e),W=!0)},o(e){Ie(w.$$.fragment,e),Ie(b.$$.fragment,e),W=!1},d(e){a(u),e&&a(B),e&&a(_),Ue(w),e&&a(N),e&&a(E),e&&a(T),e&&a(v),e&&a(D),e&&a($),e&&a(G),e&&a(z),e&&a(O),e&&a(L),e&&a(M),Ue(b,e)}}}const He={local:"checkpointing",title:"Checkpointing"};function Je(fe){return Oe(()=>{new URLSearchParams(window.location.search).get("fw")}),[]}class Xe extends Be{constructor(u){super();Ne(this,u,Je,Fe,Te,{})}}export{Xe as default,He as metadata};
---
+++ accelerate/v0.7.0/en/_app/pages/index.mdx-5d9f2c28.js
@@ -0,0 +1,113 @@
+import{S as ba,i as Pa,s as ka,e as o,k as n,w as f,t as p,M as Ea,c as i,d as t,m as c,a as s,x as y,h as m,b as u,F as a,g as l,y as _,L as Aa,q as v,o as w,B as g,v as za}from"../chunks/vendor-4918fc3c.js";import{I as Pe}from"../chunks/IconCopyLink-21d338b1.js";import{C as Z}from"../chunks/CodeBlock-99419108.js";function ja(Mt){let $,ke,b,z,ee,N,tt,te,at,Ee,j,rt,ae,ot,it,Ae,P,U,re,C,st,oe,lt,ze,x,ie,se,nt,ct,le,ne,pt,je,k,S,ce,G,mt,pe,ut,Ue,B,dt,xe,F,Se,Y,ht,Le,q,Te,K,ft,Ie,Q,yt,Ne,M,Ce,E,L,me,H,_t,ue,vt,Ge,T,wt,de,gt,$t,Fe,V,bt,qe,J,Me,W,Pt,He,O,Je,X,kt,Oe,R,Re,A,I,he,D,Et,fe,At,De,d,ye,zt,jt,_e,Ut,xt,ve,St,Lt,we,Tt,It,ge,Nt,Ct,$e,Gt,Ft,be,qt,Be;return N=new Pe({}),C=new Pe({}),G=new Pe({}),F=new Z({props:{code:`my_model.to(device)
+
+for batch in my_training_dataloader:
+    my_optimizer.zero_grad()
+    inputs, targets = batch
+    inputs = inputs.to(device)
+    targets = targets.to(device)
+    outputs = my_model(inputs)
+    loss = my_loss_function(outputs, targets)
+    loss.backward()
+    my_optimizer.step()`,highlighted:`my_model.to(device)
+
+<span class="hljs-keyword">for</span> batch <span class="hljs-keyword">in</span> my_training_dataloader:
+    my_optimizer.zero_grad()
+    inputs, targets = batch
+    inputs = inputs.to(device)
+    targets = targets.to(device)
+    outputs = my_model(inputs)
+    loss = my_loss_function(outputs, targets)
+    loss.backward()
+    my_optimizer.step()`}}),q=new Z({props:{code:`+ from accelerate import Accelerator
+
++ accelerator = Accelerator()
+  # Use the device given by the *accelerator* object.
++ device = accelerator.device
+  my_model.to(device)
+  # Pass every important object (model, optimizer, dataloader) to *accelerator.prepare*
++ my_model, my_optimizer, my_training_dataloader = accelerate.prepare(
++     my_model, my_optimizer, my_training_dataloader
++ )
+
+  for batch in my_training_dataloader:
+      my_optimizer.zero_grad()
+      inputs, targets = batch
+      inputs = inputs.to(device)
+      targets = targets.to(device)
+      outputs = my_model(inputs)
+      loss = my_loss_function(outputs, targets)
+      # Just a small change for the backward instruction
+-     loss.backward()
++     accelerator.backward(loss)
+      my_optimizer.step()`,highlighted:`<span class="hljs-addition">+ from accelerate import Accelerator</span>
+
+<span class="hljs-addition">+ accelerator = Accelerator()</span>
+  # Use the device given by the *accelerator* object.
+<span class="hljs-addition">+ device = accelerator.device</span>
+  my_model.to(device)
+  # Pass every important object (model, optimizer, dataloader) to *accelerator.prepare*
+<span class="hljs-addition">+ my_model, my_optimizer, my_training_dataloader = accelerate.prepare(</span>
+<span class="hljs-addition">+     my_model, my_optimizer, my_training_dataloader</span>
+<span class="hljs-addition">+ )</span>
+
+  for batch in my_training_dataloader:
+      my_optimizer.zero_grad()
+      inputs, targets = batch
+      inputs = inputs.to(device)
+      targets = targets.to(device)
+      outputs = my_model(inputs)
+      loss = my_loss_function(outputs, targets)
+      # Just a small change for the backward instruction
+<span class="hljs-deletion">-     loss.backward()</span>
+<span class="hljs-addition">+     accelerator.backward(loss)</span>
+      my_optimizer.step()`}}),M=new Z({props:{code:`+ from accelerate import Accelerator
+
++ accelerator = Accelerator()
+- my_model.to(device)
+  # Pass every important object (model, optimizer, dataloader) to *accelerator.prepare*
++ my_model, my_optimizer, my_training_dataloader = accelerate.prepare(
++     my_model, my_optimizer, my_training_dataloader
++ )
+
+  for batch in my_training_dataloader:
+      my_optimizer.zero_grad()
+      inputs, targets = batch
+-     inputs = inputs.to(device)
+-     targets = targets.to(device)
+      outputs = my_model(inputs)
+      loss = my_loss_function(outputs, targets)
+      # Just a small change for the backward instruction
+-     loss.backward()
++     accelerator.backward(loss)
+      my_optimizer.step()`,highlighted:`<span class="hljs-addition">+ from accelerate import Accelerator</span>
+
+<span class="hljs-addition">+ accelerator = Accelerator()</span>
+<span class="hljs-deletion">- my_model.to(device)</span>
+  # Pass every important object (model, optimizer, dataloader) to *accelerator.prepare*
+<span class="hljs-addition">+ my_model, my_optimizer, my_training_dataloader = accelerate.prepare(</span>
+<span class="hljs-addition">+     my_model, my_optimizer, my_training_dataloader</span>
+<span class="hljs-addition">+ )</span>
+
+  for batch in my_training_dataloader:
+      my_optimizer.zero_grad()
+      inputs, targets = batch
+<span class="hljs-deletion">-     inputs = inputs.to(device)</span>
+<span class="hljs-deletion">-     targets = targets.to(device)</span>
+      outputs = my_model(inputs)
+      loss = my_loss_function(outputs, targets)
+      # Just a small change for the backward instruction
+<span class="hljs-deletion">-     loss.backward()</span>
+<span class="hljs-addition">+     accelerator.backward(loss)</span>
+      my_optimizer.step()`}}),H=new Pe({}),J=new Z({props:{code:"accelerate config",highlighted:"accelerate config"}}),O=new Z({props:{code:"accelerate launch my_script.py --args_to_my_script",highlighted:"accelerate launch my_script.py --args_to_my_script"}}),R=new Z({props:{code:"accelerate launch examples/nlp_example.py",highlighted:"accelerate launch examples/nlp_example.py"}}),D=new Pe({}),{c(){$=o("meta"),ke=n(),b=o("h1"),z=o("a"),ee=o("span"),f(N.$$.fragment),tt=n(),te=o("span"),at=p("Accelerate"),Ee=n(),j=o("p"),rt=p("Run your "),ae=o("em"),ot=p("raw"),it=p(" PyTorch training script on any kind of device"),Ae=n(),P=o("h2"),U=o("a"),re=o("span"),f(C.$$.fragment),st=n(),oe=o("span"),lt=p("Features"),ze=n(),x=o("ul"),ie=o("li"),se=o("p"),nt=p(`\u{1F917} Accelerate provides an easy API to make your scripts run with mixed precision and on any kind of distributed
+setting (multi-GPUs, TPUs etc.) while still letting you write your own training loop. The same code can then runs
+seamlessly on your local machine for debugging or your training environment.`),ct=n(),le=o("li"),ne=o("p"),pt=p(`\u{1F917} Accelerate also provides a CLI tool that allows you to quickly configure and test your training environment then
+launch the scripts.`),je=n(),k=o("h2"),S=o("a"),ce=o("span"),f(G.$$.fragment),mt=n(),pe=o("span"),ut=p("Easy to integrate"),Ue=n(),B=o("p"),dt=p("A traditional training loop in PyTorch looks like this:"),xe=n(),f(F.$$.fragment),Se=n(),Y=o("p"),ht=p("Changing it to work with accelerate is really easy and only adds a few lines of code:"),Le=n(),f(q.$$.fragment),Te=n(),K=o("p"),ft=p("and with this, your script can now run in a distributed environment (multi-GPU, TPU)."),Ie=n(),Q=o("p"),yt=p(`You can even simplify your script a bit by letting \u{1F917} Accelerate handle the device placement for you (which is safer,
+especially for TPU training):`),Ne=n(),f(M.$$.fragment),Ce=n(),E=o("h2"),L=o("a"),me=o("span"),f(H.$$.fragment),_t=n(),ue=o("span"),vt=p("Script launcher"),Ge=n(),T=o("p"),wt=p("No need to remember how to use "),de=o("code"),gt=p("torch.distributed.launch"),$t=p(` or to write a specific launcher for TPU training! \u{1F917}
+Accelerate comes with a CLI tool that will make your life easier when launching distributed scripts.`),Fe=n(),V=o("p"),bt=p("On your machine(s) just run:"),qe=n(),f(J.$$.fragment),Me=n(),W=o("p"),Pt=p(`and answer the questions asked. This will generate a config file that will be used automatically to properly set the
+default options when doing`),He=n(),f(O.$$.fragment),Je=n(),X=o("p"),kt=p("For instance, here is how you would run the NLP example (from the root of the repo):"),Oe=n(),f(R.$$.fragment),Re=n(),A=o("h2"),I=o("a"),he=o("span"),f(D.$$.fragment),Et=n(),fe=o("span"),At=p("Supported integrations"),De=n(),d=o("ul"),ye=o("li"),zt=p("CPU only"),jt=n(),_e=o("li"),Ut=p("single GPU"),xt=n(),ve=o("li"),St=p("multi-GPU on one node (machine)"),Lt=n(),we=o("li"),Tt=p("multi-GPU on several nodes (machines)"),It=n(),ge=o("li"),Nt=p("TPU"),Ct=n(),$e=o("li"),Gt=p("FP16 with native AMP (apex on the roadmap)"),Ft=n(),be=o("li"),qt=p("DeepSpeed (experimental support)"),this.h()},l(e){const r=Ea('[data-svelte="svelte-1phssyn"]',document.head);$=i(r,"META",{name:!0,content:!0}),r.forEach(t),ke=c(e),b=i(e,"H1",{class:!0});var Ye=s(b);z=i(Ye,"A",{id:!0,class:!0,href:!0});var Ht=s(z);ee=i(Ht,"SPAN",{});var Jt=s(ee);y(N.$$.fragment,Jt),Jt.forEach(t),Ht.forEach(t),tt=c(Ye),te=i(Ye,"SPAN",{});var Ot=s(te);at=m(Ot,"Accelerate"),Ot.forEach(t),Ye.forEach(t),Ee=c(e),j=i(e,"P",{});var Ke=s(j);rt=m(Ke,"Run your "),ae=i(Ke,"EM",{});var Rt=s(ae);ot=m(Rt,"raw"),Rt.forEach(t),it=m(Ke," PyTorch training script on any kind of device"),Ke.forEach(t),Ae=c(e),P=i(e,"H2",{class:!0});var Qe=s(P);U=i(Qe,"A",{id:!0,class:!0,href:!0});var Dt=s(U);re=i(Dt,"SPAN",{});var Bt=s(re);y(C.$$.fragment,Bt),Bt.forEach(t),Dt.forEach(t),st=c(Qe),oe=i(Qe,"SPAN",{});var Yt=s(oe);lt=m(Yt,"Features"),Yt.forEach(t),Qe.forEach(t),ze=c(e),x=i(e,"UL",{});var Ve=s(x);ie=i(Ve,"LI",{});var Kt=s(ie);se=i(Kt,"P",{});var Qt=s(se);nt=m(Qt,`\u{1F917} Accelerate provides an easy API to make your scripts run with mixed precision and on any kind of distributed
+setting (multi-GPUs, TPUs etc.) while still letting you write your own training loop. The same code can then runs
+seamlessly on your local machine for debugging or your training environment.`),Qt.forEach(t),Kt.forEach(t),ct=c(Ve),le=i(Ve,"LI",{});var Vt=s(le);ne=i(Vt,"P",{});var Wt=s(ne);pt=m(Wt,`\u{1F917} Accelerate also provides a CLI tool that allows you to quickly configure and test your training environment then
+launch the scripts.`),Wt.forEach(t),Vt.forEach(t),Ve.forEach(t),je=c(e),k=i(e,"H2",{class:!0});var We=s(k);S=i(We,"A",{id:!0,class:!0,href:!0});var Xt=s(S);ce=i(Xt,"SPAN",{});var Zt=s(ce);y(G.$$.fragment,Zt),Zt.forEach(t),Xt.forEach(t),mt=c(We),pe=i(We,"SPAN",{});var ea=s(pe);ut=m(ea,"Easy to integrate"),ea.forEach(t),We.forEach(t),Ue=c(e),B=i(e,"P",{});var ta=s(B);dt=m(ta,"A traditional training loop in PyTorch looks like this:"),ta.forEach(t),xe=c(e),y(F.$$.fragment,e),Se=c(e),Y=i(e,"P",{});var aa=s(Y);ht=m(aa,"Changing it to work with accelerate is really easy and only adds a few lines of code:"),aa.forEach(t),Le=c(e),y(q.$$.fragment,e),Te=c(e),K=i(e,"P",{});var ra=s(K);ft=m(ra,"and with this, your script can now run in a distributed environment (multi-GPU, TPU)."),ra.forEach(t),Ie=c(e),Q=i(e,"P",{});var oa=s(Q);yt=m(oa,`You can even simplify your script a bit by letting \u{1F917} Accelerate handle the device placement for you (which is safer,
+especially for TPU training):`),oa.forEach(t),Ne=c(e),y(M.$$.fragment,e),Ce=c(e),E=i(e,"H2",{class:!0});var Xe=s(E);L=i(Xe,"A",{id:!0,class:!0,href:!0});var ia=s(L);me=i(ia,"SPAN",{});var sa=s(me);y(H.$$.fragment,sa),sa.forEach(t),ia.forEach(t),_t=c(Xe),ue=i(Xe,"SPAN",{});var la=s(ue);vt=m(la,"Script launcher"),la.forEach(t),Xe.forEach(t),Ge=c(e),T=i(e,"P",{});var Ze=s(T);wt=m(Ze,"No need to remember how to use "),de=i(Ze,"CODE",{});var na=s(de);gt=m(na,"torch.distributed.launch"),na.forEach(t),$t=m(Ze,` or to write a specific launcher for TPU training! \u{1F917}
+Accelerate comes with a CLI tool that will make your life easier when launching distributed scripts.`),Ze.forEach(t),Fe=c(e),V=i(e,"P",{});var ca=s(V);bt=m(ca,"On your machine(s) just run:"),ca.forEach(t),qe=c(e),y(J.$$.fragment,e),Me=c(e),W=i(e,"P",{});var pa=s(W);Pt=m(pa,`and answer the questions asked. This will generate a config file that will be used automatically to properly set the
+default options when doing`),pa.forEach(t),He=c(e),y(O.$$.fragment,e),Je=c(e),X=i(e,"P",{});var ma=s(X);kt=m(ma,"For instance, here is how you would run the NLP example (from the root of the repo):"),ma.forEach(t),Oe=c(e),y(R.$$.fragment,e),Re=c(e),A=i(e,"H2",{class:!0});var et=s(A);I=i(et,"A",{id:!0,class:!0,href:!0});var ua=s(I);he=i(ua,"SPAN",{});var da=s(he);y(D.$$.fragment,da),da.forEach(t),ua.forEach(t),Et=c(et),fe=i(et,"SPAN",{});var ha=s(fe);At=m(ha,"Supported integrations"),ha.forEach(t),et.forEach(t),De=c(e),d=i(e,"UL",{});var h=s(d);ye=i(h,"LI",{});var fa=s(ye);zt=m(fa,"CPU only"),fa.forEach(t),jt=c(h),_e=i(h,"LI",{});var ya=s(_e);Ut=m(ya,"single GPU"),ya.forEach(t),xt=c(h),ve=i(h,"LI",{});var _a=s(ve);St=m(_a,"multi-GPU on one node (machine)"),_a.forEach(t),Lt=c(h),we=i(h,"LI",{});var va=s(we);Tt=m(va,"multi-GPU on several nodes (machines)"),va.forEach(t),It=c(h),ge=i(h,"LI",{});var wa=s(ge);Nt=m(wa,"TPU"),wa.forEach(t),Ct=c(h),$e=i(h,"LI",{});var ga=s($e);Gt=m(ga,"FP16 with native AMP (apex on the roadmap)"),ga.forEach(t),Ft=c(h),be=i(h,"LI",{});var $a=s(be);qt=m($a,"DeepSpeed (experimental support)"),$a.forEach(t),h.forEach(t),this.h()},h(){u($,"name","hf:doc:metadata"),u($,"content",JSON.stringify(Ua)),u(z,"id","accelerate"),u(z,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),u(z,"href","#accelerate"),u(b,"class","relative group"),u(U,"id","features"),u(U,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),u(U,"href","#features"),u(P,"class","relative group"),u(S,"id","easy-to-integrate"),u(S,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),u(S,"href","#easy-to-integrate"),u(k,"class","relative group"),u(L,"id","script-launcher"),u(L,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),u(L,"href","#script-launcher"),u(E,"class","relative group"),u(I,"id","supported-integrations"),u(I,"class","header-link block pr-1.5 text-lg no-hover:hidden with-hover:absolute with-hover:p-1.5 with-hover:opacity-0 with-hover:group-hover:opacity-100 with-hover:right-full"),u(I,"href","#supported-integrations"),u(A,"class","relative group")},m(e,r){a(document.head,$),l(e,ke,r),l(e,b,r),a(b,z),a(z,ee),_(N,ee,null),a(b,tt),a(b,te),a(te,at),l(e,Ee,r),l(e,j,r),a(j,rt),a(j,ae),a(ae,ot),a(j,it),l(e,Ae,r),l(e,P,r),a(P,U),a(U,re),_(C,re,null),a(P,st),a(P,oe),a(oe,lt),l(e,ze,r),l(e,x,r),a(x,ie),a(ie,se),a(se,nt),a(x,ct),a(x,le),a(le,ne),a(ne,pt),l(e,je,r),l(e,k,r),a(k,S),a(S,ce),_(G,ce,null),a(k,mt),a(k,pe),a(pe,ut),l(e,Ue,r),l(e,B,r),a(B,dt),l(e,xe,r),_(F,e,r),l(e,Se,r),l(e,Y,r),a(Y,ht),l(e,Le,r),_(q,e,r),l(e,Te,r),l(e,K,r),a(K,ft),l(e,Ie,r),l(e,Q,r),a(Q,yt),l(e,Ne,r),_(M,e,r),l(e,Ce,r),l(e,E,r),a(E,L),a(L,me),_(H,me,null),a(E,_t),a(E,ue),a(ue,vt),l(e,Ge,r),l(e,T,r),a(T,wt),a(T,de),a(de,gt),a(T,$t),l(e,Fe,r),l(e,V,r),a(V,bt),l(e,qe,r),_(J,e,r),l(e,Me,r),l(e,W,r),a(W,Pt),l(e,He,r),_(O,e,r),l(e,Je,r),l(e,X,r),a(X,kt),l(e,Oe,r),_(R,e,r),l(e,Re,r),l(e,A,r),a(A,I),a(I,he),_(D,he,null),a(A,Et),a(A,fe),a(fe,At),l(e,De,r),l(e,d,r),a(d,ye),a(ye,zt),a(d,jt),a(d,_e),a(_e,Ut),a(d,xt),a(d,ve),a(ve,St),a(d,Lt),a(d,we),a(we,Tt),a(d,It),a(d,ge),a(ge,Nt),a(d,Ct),a(d,$e),a($e,Gt),a(d,Ft),a(d,be),a(be,qt),Be=!0},p:Aa,i(e){Be||(v(N.$$.fragment,e),v(C.$$.fragment,e),v(G.$$.fragment,e),v(F.$$.fragment,e),v(q.$$.fragment,e),v(M.$$.fragment,e),v(H.$$.fragment,e),v(J.$$.fragment,e),v(O.$$.fragment,e),v(R.$$.fragment,e),v(D.$$.fragment,e),Be=!0)},o(e){w(N.$$.fragment,e),w(C.$$.fragment,e),w(G.$$.fragment,e),w(F.$$.fragment,e),w(q.$$.fragment,e),w(M.$$.fragment,e),w(H.$$.fragment,e),w(J.$$.fragment,e),w(O.$$.fragment,e),w(R.$$.fragment,e),w(D.$$.fragment,e),Be=!1},d(e){t($),e&&t(ke),e&&t(b),g(N),e&&t(Ee),e&&t(j),e&&t(Ae),e&&t(P),g(C),e&&t(ze),e&&t(x),e&&t(je),e&&t(k),g(G),e&&t(Ue),e&&t(B),e&&t(xe),g(F,e),e&&t(Se),e&&t(Y),e&&t(Le),g(q,e),e&&t(Te),e&&t(K),e&&t(Ie),e&&t(Q),e&&t(Ne),g(M,e),e&&t(Ce),e&&t(E),g(H),e&&t(Ge),e&&t(T),e&&t(Fe),e&&t(V),e&&t(qe),g(J,e),e&&t(Me),e&&t(W),e&&t(He),g(O,e),e&&t(Je),e&&t(X),e&&t(Oe),g(R,e),e&&t(Re),e&&t(A),g(D),e&&t(De),e&&t(d)}}}const Ua={local:"accelerate",sections:[{local:"features",title:"Features"},{local:"easy-to-integrate",title:"Easy to integrate"},{local:"script-launcher",title:"Script launcher"},{local:"supported-integrations",title:"Supported integrations"}],title:"Accelerate"};function xa(Mt){return za(()=>{new URLSearchParams(window.location.search).get("fw")}),[]}class Ia extends ba{constructor($){super();Pa(this,$,xa,ja,ka,{})}}export{Ia as default,Ua as metadata};
