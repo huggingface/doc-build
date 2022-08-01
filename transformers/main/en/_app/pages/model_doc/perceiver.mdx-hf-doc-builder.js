05ac42bcc5b8ac906611a0bac72d8a40e115589f
Doc builder <97597362+HuggingFaceDocBuilder@users.noreply.github.com>
2022-08-01 14:26:59 +0200

    Updated with commit 1cd7c6f154014f62427ed3c59e14f6f503988431 See: https://github.com/huggingface/transformers/commit/1cd7c6f154014f62427ed3c59e14f6f503988431

--- transformers/main/en/_app/pages/model_doc/perceiver.mdx-hf-doc-builder.js
+++ transformers/main/en/_app/pages/model_doc/perceiver.mdx-hf-doc-builder.js
@@ -596,7 +596,7 @@ Whether or not to return <code>(char_start, char_end)</code> for each token.</p>
 Python&#x2019;s tokenizer, this method will raise <code>NotImplementedError</code>.`,name:"return_offsets_mapping"},{anchor:"transformers.PerceiverTokenizer.__call__.return_length",description:`<strong>return_length</strong>  (<code>bool</code>, <em>optional</em>, defaults to <code>False</code>) &#x2014;
 Whether or not to return the lengths of the encoded inputs.`,name:"return_length"},{anchor:"transformers.PerceiverTokenizer.__call__.verbose",description:`<strong>verbose</strong> (<code>bool</code>, <em>optional</em>, defaults to <code>True</code>) &#x2014;
 Whether or not to print more information and warnings.
-**kwargs &#x2014; passed to the <code>self.tokenize()</code> method`,name:"verbose"}],source:"https://github.com/huggingface/transformers/blob/main/src/transformers/tokenization_utils_base.py#L2431",returnDescription:`
+**kwargs &#x2014; passed to the <code>self.tokenize()</code> method`,name:"verbose"}],source:"https://github.com/huggingface/transformers/blob/main/src/transformers/tokenization_utils_base.py#L2437",returnDescription:`
 <p>A <a
   href="/docs/transformers/main/en/main_classes/tokenizer#transformers.BatchEncoding"
 >BatchEncoding</a> with the following fields:</p>
