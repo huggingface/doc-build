6dfdb0bb842aea08a95a312186be3cd690f029ef
Doc builder <97597362+HuggingFaceDocBuilder@users.noreply.github.com>
2022-10-31 10:07:36 -0400

    Updated with commit bf7b0bc25b4a10da78ae9dd19ae00ba840585d40 See: https://github.com/huggingface/diffusers/commit/bf7b0bc25b4a10da78ae9dd19ae00ba840585d40

--- diffusers/main/en/_app/pages/api/pipelines/stable_diffusion.mdx-hf-doc-builder.js
+++ diffusers/main/en/_app/pages/api/pipelines/stable_diffusion.mdx-hf-doc-builder.js
@@ -59,7 +59,7 @@ plain tuple.`,name:"return_dict"},{anchor:"diffusers.StableDiffusionPipeline.__c
 A function that will be called every <code>callback_steps</code> steps during inference. The function will be
 called with the following arguments: <code>callback(step: int, timestep: int, latents: torch.FloatTensor)</code>.`,name:"callback"},{anchor:"diffusers.StableDiffusionPipeline.__call__.callback_steps",description:`<strong>callback_steps</strong> (<code>int</code>, <em>optional</em>, defaults to 1) &#x2014;
 The frequency at which the <code>callback</code> function will be called. If not specified, the callback will be
-called at every step.`,name:"callback_steps"}],source:"https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py#L138",returnDescription:`
+called at every step.`,name:"callback_steps"}],source:"https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py#L139",returnDescription:`
 <p><a
   href="/docs/diffusers/main/en/api/pipelines/stable_diffusion#diffusers.pipelines.stable_diffusion.StableDiffusionPipelineOutput"
 >StableDiffusionPipelineOutput</a> if <code>return_dict</code> is True, otherwise a <code>tuple. When returning a tuple, the first element is a list with the generated images, and the second element is a list of </code>bool<code>s denoting whether the corresponding generated image likely represents "not-safe-for-work" (nsfw) content, according to the </code>safety_checker\`.</p>
